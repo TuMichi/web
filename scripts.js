a3a15538015f627de8ae3b9b2eaef57d29aa387a
TuMichi <marctorres07062010@gmail.com>
2025-04-04 16:11:14 +0200

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function openImage(game) {
+    const overlay = document.getElementById("overlay");
+    const overlayImage = document.getElementById("overlay-image");
+    overlay.classList.add("active");
+    overlayImage.src = `images/${game}.jpg`;
+}
+
+function closeOverlay() {
+    const overlay = document.getElementById("overlay");
+    overlay.classList.remove("active");
+}
